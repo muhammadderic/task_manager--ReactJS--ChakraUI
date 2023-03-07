c67d4a3cb0b25954cb5769eafef9404290a19870
muhammadderic <125786806+muhammadderic@users.noreply.github.com>
2023-03-07 21:25:24 +0700

    Setup project

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # task_manager--ReactJS--ChakraUI
-
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-2t1w8v)
--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Hello Deric</h1>
     </div>
   );
 }
